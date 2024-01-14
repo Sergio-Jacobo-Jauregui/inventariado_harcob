f215c721345cde26c7eba96d7773eb2775fa78f7
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2024-01-14 12:37:22 -0500

    ADD: pysqlite3 to usein lambda

--- organization/views.py
+++ organization/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Organization
+from django.core.serializers import serialize
 
-# Create your views here.
+def get_all(self):
+  return JsonResponse({'mensaje': 'organizations'})
+
+
+def get_all(self):
+  orgs = Organization.objects.all()
+  orgs_serializado = serialize('json', orgs)
+
+  return JsonResponse(orgs_serializado, safe=False)

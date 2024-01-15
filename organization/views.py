d49ad01eb89935380c6cf06ad6c18b6c9a4517a4
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2024-01-15 13:01:47 -0500

    DELETE: unnecesary function

--- organization/views.py
+++ organization/views.py
@@ -4,11 +4,6 @@ from .models import Organization
 from django.core.serializers import serialize
 
 def get_all(self):
-  return JsonResponse({'mensaje': 'organizations'})
-
-
-def get_all(self):
   orgs = Organization.objects.all()
   orgs_serializado = serialize('json', orgs)
-
   return JsonResponse(orgs_serializado, safe=False)

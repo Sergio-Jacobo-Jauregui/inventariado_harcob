b7ffbdc0a044d977826cbfee6065492073e2d36e
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2024-01-16 16:08:18 -0500

    ADD: django-storage to use S3

--- organization/views.py
+++ organization/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from .models import Organization
 from django.core.serializers import serialize
 
-def get_all(self):
+def get_all(request):
   orgs = Organization.objects.all()
   orgs_serializado = serialize('json', orgs)
   return JsonResponse(orgs_serializado, safe=False)

467637da23f2b1dd3751132df33ecd8188025786
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2024-01-18 11:43:38 -0500

    ADD: auth urls

--- inventariado_Harcob/settings.py
+++ inventariado_Harcob/settings.py
@@ -133,3 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Log In
+LOGIN_URL = '/auth/login/'
--- inventariado_Harcob/urls.py
+++ inventariado_Harcob/urls.py
@@ -1,13 +1,14 @@
-def ok(request):
-    data = {'mensaje': 'All ok'}
-    return JsonResponse(data)
-
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+def ok(request):
+    data = {'mensaje': 'All ok'}
+    return JsonResponse(data)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('ok/', ok),
     path('orgs/', include('organization.urls')),
 ]

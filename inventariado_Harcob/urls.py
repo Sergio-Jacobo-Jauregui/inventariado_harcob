3ba7f3eab0bb79397779d8606d56bcc228fa8322
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2024-01-18 12:47:18 -0500

    ADD: login and logout path

--- inventariado_Harcob/urls.py
+++ inventariado_Harcob/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.contrib.auth.views import LoginView, LogoutView
 
 def ok(request):
     data = {'mensaje': 'All ok'}
@@ -11,4 +12,6 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('ok/', ok),
     path('orgs/', include('organization.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]

29f81c151ecd428189dbb4e577d3799a26173ead
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2024-01-19 10:51:30 -0500

    ADD: models to admin

--- accion_record/admin.py
+++ accion_record/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import AccionRecord
 
-# Register your models here.
+admin.site.register(AccionRecord)
--- purchase_record/admin.py
+++ purchase_record/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import PurchaseRecord
 
-# Register your models here.
+admin.site.register(PurchaseRecord)
